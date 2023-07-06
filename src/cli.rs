e7a322a3ec229658a6baf7b34afb8b25f9cfcd9e
Gary Holland <gbGarHol@MacBook-Pro-7.local>
2023-07-06 11:55:27 +0100

    chore: document inputs

--- src/cli.rs
+++ src/cli.rs
@@ -4,9 +4,14 @@ use humantime::Duration;
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 pub struct Args {
-    /// Human-readable timer duration, e.g. "20 minutes" or "2h 30m"
+    /// Length of the work period in human-readable format, e.g. "20min", "1hr 30min" etc
+    ///
+    /// Defaults to 20 minutes
     #[arg(short, long, default_value = "20 minutes")]
     pub work: Duration,
+    /// Length of the rest period in human-readable format, e.g. "20min", "1hr 30min" etc
+    ///
+    /// Defaults to 5 minutes
     #[arg(short, long, default_value = "5 minutes")]
     pub rest: Duration,
 }
